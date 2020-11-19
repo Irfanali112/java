f825a5371b0fdce409f2fb93c323864ca082f917
Irfanali112 <61146331+Irfanali112@users.noreply.github.com>
2020-11-19 18:31:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+
+
+// var name="irfan ali";
+
+
+// alert(name);
+
+// var fathername="m.siddique";
+
+// alert(fathername)
+
+// var nationality="pakistan";
+
+// alert(nationality);
+
+
+// var a = "fgfdgfdgfdg"
+// var b =   123;
+
+
+// console.log(a);
+// console.log(b);
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+///////////////////////
+// let num1 = 16;
+// let num2 = 3;
+
+// let result = num1 + num2;
+
+// result =  1 + result
+
+// console.log(result);
+
+
+// var a ="irfan";
+// var b="ali";
+// var fullname=a+"  "+b
+
+
+// console.log(fullname)
+
+
+
+// let a = 24;
+// let b = ++a ;
+
+
+// console.log ("result",b);
+
+
+
+let a = 24;
+let b = a ;
+
+
+console.log ("result",b);
